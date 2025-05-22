465dcec629110f13c81818747d6b680ea2450066
aigle-levant <aiglelevant@gmail.com>
2025-05-22 21:04:53 +0530

    Modified Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@
 import React from "react";
 // components
 import HeroSection from "../components/home/HeroSection";
-import AnimatedSection from '../components/home/AnimatedSection';
 
 export default function Home () {
     return (
-        <div>
+        <div id="home">
             <HeroSection/>
-            <AnimatedSection/>
         </div>
     )
 }
